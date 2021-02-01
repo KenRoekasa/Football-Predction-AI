6ea3bb30178f97922b10b9935a636169ef0a3de0
KennyR <kennykr@hotmail.co.uk>
2021-02-01 17:47:28 +0000

    Add pi-rating

---
+++ pi_rating.py
@@ -0,0 +1,54 @@
+import math
+
+
+def weighted_error(e):
+    c = 3
+    return c * math.log(1 + e, 10)
+
+
+def pi_rating(Rah, Raa, Rbh, Rba, a_score, b_score):
+    lambda_rate = 0.1
+    gamma = 0.3
+    b = 10
+    c = 3
+
+    goal_difference = a_score - b_score
+
+    # Calculate the expected goal differences
+    expected_goal_difference_a = pow(b, (math.fabs(Rah) / c)) - 1
+    expected_goal_difference_b = - (pow(b, (math.fabs(Rba) / c)) - 1)
+
+    # Calculate the expected goal difference
+    predicted_goal_difference = expected_goal_difference_a - expected_goal_difference_b
+
+    e = math.fabs(predicted_goal_difference - goal_difference)
+
+    if predicted_goal_difference < goal_difference:
+        weighted_e_a = weighted_error(e)
+    else:
+        weighted_e_a = -weighted_error(e)
+
+    if predicted_goal_difference > goal_difference:
+        weighted_e_b = weighted_error(e)
+    else:
+        weighted_e_b = -weighted_error(e)
+
+        # update home team's rating
+    Rah_new = Rah + weighted_e_a * lambda_rate
+    Raa_new = Raa + (Rah_new - Rah) * gamma
+
+    # update away team's rating
+    Rba_new = Rba + weighted_e_b * lambda_rate
+    Rbh_new = Rbh + (Rba_new - Rba) * gamma
+
+    print("%f, %f, %f,%f" % (Rah_new, Raa_new, Rba_new, Rbh_new))
+
+    return Rah_new, Raa_new, Rba_new, Rbh_new
+
+
+if __name__ == "__main__":
+    Rah, Raa, Rbh, Rba = pi_rating(1.6, 0.4, 0.3, -1.2, 4, 1)
+    print(Rah)
+    print(Raa)
+    print(Rbh)
+    print(Rba)
---
+++ pi_rating_gen.py
@@ -0,0 +1,58 @@
+import csv
+
+from pi_rating import pi_rating
+import json
+
+pi_ratings = {}
+league__csv = "data/england-premier-league-17-2.csv"
+ratings_json = 'data/pi_ratings.json'
+try:
+    with open(ratings_json) as json_file:
+        pi_ratings = json.load(json_file)
+except:
+    pass
+
+results = []
+
+with open(league__csv, mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        results.append(row)
+
+        if row[1] not in pi_ratings:
+            pi_ratings[row[1]] = {}
+            pi_ratings[row[1]]['home'] = 0
+            pi_ratings[row[1]]['away'] = 0
+
+print(pi_ratings)
+
+for row in results:
+    teama = row[1]
+    teamb = row[2]
+    ascore = row[4]
+    bscore = row[5]
+    # print(elo_ratings)
+
+    Rah, Raa, Rbh, Rba = pi_rating(float(pi_ratings[teama]['home']), float(pi_ratings[teama]['away']),
+                                   float(pi_ratings[teamb]['home']),
+                                   float(pi_ratings[teamb]['away']), int(ascore), int(bscore))
+
+    pi_ratings[teama]['home'] = Rah
+    pi_ratings[teama]['away'] = Raa
+    pi_ratings[teamb]['home'] = Rbh
+    pi_ratings[teamb]['away'] = Rba
+
+    row.append(Rah)
+    row.append(Raa)
+    row.append(Rbh)
+    row.append(Rba)
+
+print(pi_ratings)
+
+with open(league__csv, 'w+', newline='') as myfile:
+    wr = csv.writer(myfile, delimiter=',')
+    wr.writerows(results)
+
+with open(ratings_json, 'w') as fp:
+    json.dump(pi_ratings, fp)
