bd49c97621da5687580389f82dca23c4426fc1be
KennyR <kennykr@hotmail.co.uk>
2021-02-04 22:17:26 +0000

    Add whoscored get match links script

---
+++ who_scored_get_match_links.py
@@ -0,0 +1,56 @@
+import csv
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+import sys
+
+if __name__ == "__main__":
+
+    # filepath = "data/whoscored/match-links.csv"
+    # league_url = "https://www.whoscored.com/Regions/252/Tournaments/2/Seasons/7811/England-Premier-League"
+
+    if len(sys.argv) == 3:
+        filepath = sys.argv[1]
+        league_url = sys.argv[2]
+
+        chromepath = "chromedriver.exe"
+        driver = webdriver.Chrome(chromepath)
+        driver.maximize_window()
+
+        driver.get(league_url)
+        time.sleep(1)
+
+        # accept cookies
+
+        cookie_button = driver.find_element_by_xpath("//div[@class='qc-cmp2-summary-buttons']/button[2]")
+        cookie_button.click()
+
+        match_links = []
+
+        previous_button = driver.find_element_by_xpath("//a[@class='previous button ui-state-default rc-l is-default']")
+
+        while previous_button is not None:
+            soup = BeautifulSoup(driver.page_source, 'lxml')
+            match_report_list = soup.find_all('a', class_="match-link match-report rc")
+
+            # Get links for matches
+            for match in match_report_list:
+                match_links.append(match['href'])
+                print(match['href'])
+            try:
+                previous_button = driver.find_element_by_xpath(
+                    "//a[@class='previous button ui-state-default rc-l is-default']")
+                previous_button.click()
+            except NoSuchElementException:
+                break
+            time.sleep(1)
+        driver.close()
+        with open(filepath, 'w+', newline='') as myfile:
+            wr = csv.writer(myfile, delimiter=',')
+            wr.writerow(match_links)
+
+    else:
+        print("Invalid arguments who_scored_get_match_links.py [csv file name] [league link]")
