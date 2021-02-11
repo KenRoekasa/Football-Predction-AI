b5877f2c12cf7177d9918a5b91569b3c67e320f5
KennyR <kennykr@hotmail.co.uk>
2021-02-11 16:28:01 +0000

    Add ease of config to network

--- model/neural.py
+++ model/neural.py
@@ -1,9 +1,9 @@
-import time
 
 import numpy as np
-import tensorflow as tf
 
-from data_preparation.dataloader import load_premier_league_data
+from data_preparation.dataloader import load_training_data
+import tensorflow as tf
+import config
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 
@@ -12,12 +12,12 @@ try:
         tf.config.experimental.set_memory_growth(gpu, True)
 except RuntimeError as e:
     print(e)
-NAME = 'model-%d' % int(time.time())
-tensorboard = tf.keras.callbacks.TensorBoard(log_dir="logs/{}".format(NAME))
+
+tensorboard = tf.keras.callbacks.TensorBoard(log_dir="logs/{}".format(config.NAME))
 
 
 def prepare_data():
-    training_data = load_premier_league_data()  # Get premier league data
+    training_data = load_training_data('../data/whoscored/alltrainingdata.pickle')  # Get premier league data
     # random.shuffle(training_data)
     x = []  # features set
     y = []  # label set
@@ -25,8 +25,13 @@ def prepare_data():
         x.append(features)
         y.append(label)
     X = np.array(x)
-    X = X / X.max(axis=0)
+
+    # Normalise the values
+
+    X = (X-X.min(axis=0))/ (X.max(axis=0)-X.min(axis=0))
+
     # print(X[0])
+
     y = np.array(y)
     # X = tf.keras.utils.normalize(X, axis=1)
     return X, y
@@ -37,23 +42,20 @@ x_train, y_train = prepare_data()
 
 model = tf.keras.models.Sequential()
 
-model.add(tf.keras.layers.Dense(64, activation=tf.nn.relu, input_dim=46))
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(64, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(16, activation=tf.nn.relu))
+for layer in config.network_topology:
+    model.add(tf.keras.layers.Dense(layer, activation=tf.nn.relu, input_dim=config.INPUT_DIM))
+# model.add(tf.keras.layers.Dropout(0.2))
 model.add(tf.keras.layers.Dense(3, activation=tf.nn.softmax))
 
 
+
 # plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-opt = tf.keras.optimizers.SGD(lr=0.001)
-# opt = tf.keras.optimizers.Adam(lr=0.05)
-model.compile(optimizer=opt, loss='sparse_categorical_crossentropy',
+
+# opt = tf.keras.optimizers.Adam(lr=0.001)
+model.compile(optimizer=config.opt, loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=100, batch_size=10, shuffle=True, callbacks=[tensorboard])
+model.fit(x_train, y_train, epochs=500, batch_size=config.batch_size, shuffle=True, callbacks=[tensorboard], validation_split=0.1)
 
 # val_loss, val_acc = model.evaluate(x_test, y_test)
 # print(val_loss, val_acc)
