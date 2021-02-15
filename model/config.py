a50da00df58cfe20206e8cd92fdcee26f2932b8c
kxr758 <kxr758@student.bham.ac.uk>
2021-02-15 17:39:40 +0000

    Create hyperparameter testing for a single neuron

--- model/config.py
+++ model/config.py
@@ -3,58 +3,53 @@ import time
 import numpy as np
 from tensorboard.plugins.hparams import api as hp
 
+INPUT_DIM = 50
+
 METRIC_ACCURACY = 'accuracy'
 
-
-HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([24, 89]))
-HP_DROPOUT = hp.HParam('dropout', hp.Discrete(np.round(np.arange(float(0.5), float(0.9), 0.1),1)))
-# HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['adam', 'sgd']))
-HP_LR = hp.HParam('learning_rate',hp.Discrete(np.round(np.arange(float(0.001), float(0.011), 0.001),3)))
-HP_BATCH_SIZE = hp.HParam('batch_size',hp.Discrete([1,2,10,32,64,100]))
-
-
-
-
+HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([24, 89, 100, INPUT_DIM]))
+HP_DROPOUT = hp.HParam('dropout', hp.Discrete([0.5, 0.6, 0.7, 0.8]))
+HP_OPTIMISER = hp.HParam('optimiser', hp.Discrete(['adam', 'sgd']))
+HP_LR = hp.HParam('learning_rate', hp.Discrete(np.round(np.arange(float(0.001), float(0.011), 0.001), 3).tolist()))
+HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([1, 2, 12, 32, 64, 100]))
+HP_MOMENTUM = hp.HParam('momentum', hp.Discrete([0]))
 
 new_data = False
 
-NETWORK_TOPOLOGY=[24]
+NETWORK_TOPOLOGY = [24]
 
 DROPOUT_VALUE = 0.8
 
 LEARNING_RATE = 0.001
-MOMENTUM = 0.01
+MOMENTUM = 0
 BATCH_SIZE = 50
 time = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
-NAME = 'model-%s-lr=%f-momentum=%f-batch-size=%d-dropout=%d-24' % (time, LEARNING_RATE,MOMENTUM,BATCH_SIZE,DROPOUT_VALUE)
-
+NAME = 'model-%s-lr=%f-momentum=%f-batch-size=%d-dropout=%d-24' % (
+    time, LEARNING_RATE, MOMENTUM, BATCH_SIZE, DROPOUT_VALUE)
 
 N_PREVIOUS_GAMES = 3
 
-
 COLUMNS = ['date', 'link', 'home team', 'away team', 'home score', 'away score', 'home total shots', 'away total shots',
-         'home shots on target', 'away shots on target', 'home possession', 'away possession',
-         'home total conversion rate',
-         'away total conversion rate', 'home fouls', 'away fouls', 'home yellow cards', 'away yellow cards',
-         'home red cards', 'away red cards', 'home total passes', 'away total passes', 'home accurate passes',
-         'away accurate passes', 'home open play conversion rate', 'away open play conversion rate',
-         'home set piece conversion', 'away set piece conversion', 'home counter attack shots',
-         'away counter attack shots',
-         'home counter attack goals', 'away counter attack goals', 'home key passes', 'away key passes',
-         'home dribbles attempted', 'away dribbles attempted', 'home dribble success', 'away dribble success',
-         'home aerials won%', 'away aerials won%', 'home tackles attempted', 'away tackles attempted',
-         'home tackles success %', 'away tackles success %', 'home was dribbled', 'away was dribbled',
-         'home interceptions',
-         'away interceptions', 'home dispossessed', 'away dispossessed', 'home errors', 'away errors',
-        'home elo',
-         'away elo'
-         ]
+           'home shots on target', 'away shots on target', 'home possession', 'away possession',
+           'home total conversion rate',
+           'away total conversion rate', 'home fouls', 'away fouls', 'home yellow cards', 'away yellow cards',
+           'home red cards', 'away red cards', 'home total passes', 'away total passes', 'home accurate passes',
+           'away accurate passes', 'home open play conversion rate', 'away open play conversion rate',
+           'home set piece conversion', 'away set piece conversion', 'home counter attack shots',
+           'away counter attack shots',
+           'home counter attack goals', 'away counter attack goals', 'home key passes', 'away key passes',
+           'home dribbles attempted', 'away dribbles attempted', 'home dribble success', 'away dribble success',
+           'home aerials won%', 'away aerials won%', 'home tackles attempted', 'away tackles attempted',
+           'home tackles success %', 'away tackles success %', 'home was dribbled', 'away was dribbled',
+           'home interceptions',
+           'away interceptions', 'home dispossessed', 'away dispossessed', 'home errors', 'away errors',
+           'home elo',
+           'away elo'
+           ]
 
-INPUT_DIM=44
 
-def combination_of_means(teama_mean_array,teamb_mean_array): # How to combine the two means find the difference or put them all into the input
+def combination_of_means(teama_mean_array,
+                         teamb_mean_array):  # How to combine the two means find the difference or put them all into the input
     mean_data_array = np.append(teama_mean_array, teamb_mean_array)
     # mean_data_array = teama_mean_array - teamb_mean_array
     return mean_data_array
-
-
--- model/neural.py
+++ model/neural.py
@@ -1,25 +1,29 @@
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import time
 
 import numpy as np
 
 from data_preparation.dataloader import load_training_data
 
 import tensorflow as tf
+import tensorflow_cloud as tfc
 import model.config as cf
 from tensorboard.plugins.hparams import api as hp
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
+# tfc.run()
 
-try:
-    for gpu in gpus:
-        tf.config.experimental.set_memory_growth(gpu, True)
-except RuntimeError as e:
-    print(e)
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+#
+# try:
+#     for gpu in gpus:
+#         tf.config.experimental.set_memory_growth(gpu, True)
+# except RuntimeError as e:
+#     print(e)
 
-tf.get_logger().setLevel('ERROR')
-tf.autograph.set_verbosity(1)
+# tf.get_logger().setLevel('ERROR')
+# tf.autograph.set_verbosity(1)
+
 
 def prepare_data():
     training_data = load_training_data('../data/whoscored/alltrainingdata.pickle')  # Get premier league data
@@ -43,49 +47,59 @@ def prepare_data():
 
 
 x_train, y_train = prepare_data()
+
+
 # x_test, y_test = prepare_data()
 
 
+def train_test_model(logdir, hparams):
 
-
-def train_test_model(logdir,hparams):
-    OPT = tf.keras.optimizers.SGD(lr=hparams[cf.HP_LR], momentum=cf.MOMENTUM)
+    if hparams[cf.HP_OPTIMISER] == "adam":
+        optimiser = tf.keras.optimizers.Adam(learning_rate=hparams[cf.HP_LR])
+    elif hparams[cf.HP_OPTIMISER] == "sgd":
+        optimiser = tf.keras.optimizers.SGD(lr=hparams[cf.HP_LR], momentum=cf.MOMENTUM)
+    else:
+        raise ValueError("unexpected optimiser name: %r" % (hparams[cf.HP_OPTIMISER],))
 
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Dense(hparams[cf.HP_NUM_UNITS], activation=tf.nn.relu, input_dim=cf.INPUT_DIM))
     model.add(tf.keras.layers.Dropout(hparams[cf.HP_DROPOUT]))
     model.add(tf.keras.layers.Dense(3, activation=tf.nn.softmax))
 
-    model.compile(optimizer=OPT, loss='sparse_categorical_crossentropy',
-            metrics=['accuracy'])
+    model.compile(optimizer=optimiser, loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
 
     callbacks = [
-        tf.keras.callbacks.TensorBoard(logdir),  # log metrics
+        tf.keras.callbacks.TensorBoard(logdir,histogram_freq=1),  # log metrics
         hp.KerasCallback(logdir, hparams),  # log hparams
     ]
 
-    model.fit(x_train, y_train, epochs=5, batch_size=hparams[cf.HP_BATCH_SIZE],shuffle=True, verbose=1,callbacks=callbacks, validation_split=0.1)
+    # model.save('saved_models/')
+    model.fit(x_train, y_train, epochs=250, batch_size=hparams[cf.HP_BATCH_SIZE], shuffle=True, verbose=1,
+              callbacks=callbacks, validation_split=0.1)
     _, accuracy = model.evaluate(x_train, y_train)
 
     return accuracy
 
-session_num = 0
-for lr in cf.HP_LR.domain.values:
-    for batch_size in cf.HP_BATCH_SIZE.domain.values:
-        for num_units in cf.HP_NUM_UNITS.domain.values:
-          for dropout_rate in cf.HP_DROPOUT.domain.values:
-              hparams = {
-                  cf.HP_NUM_UNITS: num_units,
-                  cf.HP_DROPOUT: dropout_rate,
-                  cf.HP_LR: lr,
-                  cf.HP_BATCH_SIZE: batch_size,
-              }
-              run_name = "run-%d" % session_num
-              print('--- Starting trial: %s' % run_name)
-              print({h.name: hparams[h] for h in hparams})
-              train_test_model('logs/hparam_tuning/' + run_name,hparams)
-              session_num += 1
 
+session_num = 0
+for optimiser in cf.HP_OPTIMISER.domain.values:
+    for lr in cf.HP_LR.domain.values:
+        for batch_size in cf.HP_BATCH_SIZE.domain.values:
+            for num_units in cf.HP_NUM_UNITS.domain.values:
+                for dropout_rate in cf.HP_DROPOUT.domain.values:
+                    hparams = {
+                        cf.HP_NUM_UNITS: num_units,
+                        cf.HP_DROPOUT: dropout_rate,
+                        cf.HP_LR: lr,
+                        cf.HP_BATCH_SIZE: batch_size,
+                        cf.HP_OPTIMISER: optimiser
+                    }
+                    run_name = "run-%d" % session_num
+                    print('--- Starting trial: %s' % run_name)
+                    print({h.name: hparams[h] for h in hparams})
+                    train_test_model('logs/hparam_tuning/ratio-norm/elo/dropout/single-neuron/' + run_name, hparams)
+                    session_num += 1
 
 # val_loss, val_acc = model.evaluate(x_test, y_test)
 # print(val_loss, val_acc)
