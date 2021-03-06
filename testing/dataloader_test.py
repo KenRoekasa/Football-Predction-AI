92a269b961c0bb96225443e6ef141deb09a19708
kxr758 <kxr758@student.bham.ac.uk>
2021-03-06 19:33:01 +0000

    Add edge case test for previous games

--- testing/dataloader_test.py
+++ testing/dataloader_test.py
@@ -51,6 +51,29 @@ class TestDataloader(unittest.TestCase):
         self.assertAlmostEqual(float(home_rating[1]), 0.760428496)
         self.assertAlmostEqual(float(away_rating[1]), 0.908354822)
 
+    def test_get_rating_with_no_previous_games(self):
+        settings = {'n': 4, 'columns': 'pi-rating only', 'rating normalisation': 'min-max',
+                    'combination': 'append'}
+        data = format_data(self.data, settings)
+
+        home_rating, away_rating = get_ratings(data, data.iloc[0], 'pi rating')
+
+        self.assertEqual(float(home_rating), 0)
+        self.assertEqual(float(away_rating), 0)
+
+        home_rating, away_rating = get_ratings(data, data.iloc[0], 'elo')
+
+        self.assertEqual(float(home_rating), 1000)
+        self.assertEqual(float(away_rating), 1000)
+
+        elo_home_rating, pi_home_rating, elo_away_rating, pi_away_rating = get_ratings(data, data.iloc[0], 'both')
+
+        self.assertEqual(float(elo_home_rating), 1000)
+        self.assertEqual(float(elo_away_rating), 1000)
+
+        self.assertEqual(float(pi_home_rating), 0)
+        self.assertEqual(float(pi_away_rating), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
