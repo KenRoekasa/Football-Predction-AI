d0de34be8baa883c6af694f0614543f930d7f727
kxr758 <kxr758@student.bham.ac.uk>
2021-03-28 18:12:13 +0100

    Add more tests

--- testing/dataloader_test.py
+++ testing/dataloader_test.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 from data_preparation.dataloader import get_previous_n_games, format_data, get_winstreak, normalise_ratings, \
-    get_ratings, get_mean_array, create_training_data
+    get_ratings, get_mean_array, create_training_data, get_mean_stats, get_losestreak
 from pandas._testing import assert_frame_equal
 import numpy as np
 
@@ -127,7 +127,7 @@ class TestDataloader(unittest.TestCase):
         previous_games = get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
         array = get_mean_array('Liverpool', previous_games)
 
-        np.testing.assert_array_equal(array, [2, 0])
+        np.testing.assert_array_equal(array, [6,6,0,1])
 
     def test_create_training_data(self):
         settings = {'n': 3, 'columns': 'both', 'rating normalisation': 'min-max',
@@ -136,7 +136,32 @@ class TestDataloader(unittest.TestCase):
         data = format_data(self.data, settings)
         training_data = create_training_data(data, settings)
 
-        print(training_data)
+        # print(training_data)
+
+    def test_get_mean_stats(self):
+        data = pd.DataFrame(np.array([['Chelsea', 'Arsenal', 1, 2, 3, 10], ['Liverpool', 'Chelsea', 4, 5, 6, 5],
+                                      ['Chelsea', 'Man U', 7, 8, 9, 5]]),
+                            columns=['home team', 'away team', 'home shots', 'home score',
+                                     'away shots', 'away score'])
+
+        mean_data = get_mean_stats(data, 'Chelsea')
+        np.testing.assert_array_equal(mean_data, np.array([14, 15, 20]))
+
+    def test_get_losestreak(self):
+        previous = pd.DataFrame(np.array([['Chelsea', 'Arsenal', 1, 2, 3, 10],
+                                          ['Arsenal', 'Liverpool', 4, 5, 6, 5],
+                                          ['Arsenal', 'Man U', 0, 2, 4, 25],
+                                          ['Chelsea', 'Arsenal', 2, 2, 3, 10],
+                                          ['Arsenal', 'Man U', 0, 1, 25, 5],
+                                          ['Watford', 'Arsenal', 3, 1, 4, 8],
+                                          ['Tottenham', 'Arsenal', 7, 1, 2, 12]]),
+
+                                columns=['home team', 'away team', 'home score', 'away score', 'home shots',
+                                         'away shots'])
+
+        self.assertEqual(get_losestreak(previous, 'Arsenal'),
+                         3)
+
 
 if __name__ == '__main__':
     unittest.main()
