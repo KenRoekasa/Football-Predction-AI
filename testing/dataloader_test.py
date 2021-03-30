1b5eae9a5ab8d0223a59578e7ec21d0841225404
kxr758 <kxr758@student.bham.ac.uk>
2021-03-30 17:43:14 +0100

    Add more tests for normalisation

--- testing/dataloader_test.py
+++ testing/dataloader_test.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 from data_preparation.dataloader import get_previous_n_games, format_data, get_winstreak, normalise_ratings, \
-    get_ratings, get_mean_array, create_training_data, get_mean_stats, get_losestreak
+    get_ratings, get_mean_array, create_training_data, get_mean_stats, get_losestreak, normalise_input_array
 from pandas._testing import assert_frame_equal
 import numpy as np
 
@@ -127,14 +127,14 @@ class TestDataloader(unittest.TestCase):
         previous_games = get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
         array = get_mean_array('Liverpool', previous_games)
 
-        np.testing.assert_array_equal(array, [6,6,0,1])
+        np.testing.assert_array_equal(array, [6, 6, 0, 1])
 
     def test_create_training_data(self):
         settings = {'n': 3, 'columns': 'both', 'rating normalisation': 'min-max',
                     'combination': 'append'}
 
         data = format_data(self.data, settings)
-        training_data = create_training_data(data, settings)
+        training_data = create_training_data(data,settings)
 
         # print(training_data)
 
@@ -161,6 +161,50 @@ class TestDataloader(unittest.TestCase):
 
         self.assertEqual(get_losestreak(previous, 'Arsenal'),
                          3)
+
+    def test_normalise_input_array(self):
+        x = np.array([[2, 0, 4, 1, 5, 4, 100, 280, 1.2232, 1.2245, 1.00264, 0.212],
+                      [6, 1, 0, 0, 2, 1, 96, 360, 1.22555, 0.245, 0.222, 1.666]])
+
+        y = normalise_input_array(x, 'ratio')
+
+        np.testing.assert_array_equal(y, np.array(
+            [[1, 0, 0.8, 0.2, 5 / 9, 4 / 9, 100 / 380, 280 / 380, 1.2232, 1.2245, 1.00264, 0.212],
+             [6 / 7, 1 / 7, 0.5, 0.5, 2 / 3, 1 / 3, 96 / 456, 360 / 456, 1.22555, 0.245, 0.222, 1.666]]))
+
+        x = np.array([[2, 0, 4, 1, 5, 4, 100, 280, 1.2232, 1.2245, 1.00264, 0.212],
+                      [6, 1, 0, 0, 2, 1, 96, 360, 1.22555, 0.245, 0.222, 1.666]])
+
+        real = np.empty_like(x)
+        real[0] = x[0] / np.sum(x[0])
+        real[1] = x[1] / np.sum(x[1])
+
+        y = normalise_input_array(x, 'l1')
+
+        np.testing.assert_array_equal(y, real)
+
+        x = np.array([[2, 0, 4, 1, 5, 4, 100, 280, 1.2232, 1.2245, 1.00264, 0.212],
+                      [6, 1, 0, 0, 2, 1, 96, 360, 1.22555, 0.245, 0.222, 1.666]])
+
+        real = np.empty_like(x)
+        real[0] = x[0] / (np.sqrt(np.sum(np.square(x[0]))))
+        real[1] = x[1] / (np.sqrt(np.sum(np.square(x[1]))))
+
+        y = normalise_input_array(x, 'l2')
+
+        np.testing.assert_array_equal(y, real)
+
+        x = np.array([[2, 0, 4, 1, 5, 4, 100, 280, 1.2232, 1.2245, 1.00264, 0.212],
+                  [6, 1, 0, 0, 2, 1, 96, 360, 1.456, 0.7856, 0.564, 1.456],
+                  [4, 7, 0, 4, 1, 0, 25, 125, 1.22555, 0.245, 0.222, 1.666]])
+
+        real = np.copy(x)
+
+        real[:, :-4] = (x[:, :-4] - x[:, :-4].min(axis=0)) / (x[:, :-4].max(axis=0) - x[:, :-4].min(axis=0))
+
+        y = normalise_input_array(x, 'max')
+
+        np.testing.assert_array_equal(y, real)
 
 
 if __name__ == '__main__':
