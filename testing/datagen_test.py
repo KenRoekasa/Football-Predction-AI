581c6dec668e6f1909cd5e51b581c23a33ce6d4f
kxr758 <kxr758@student.bham.ac.uk>
2021-04-08 18:18:22 +0100

    Update dataloader test to work with new datageneration class

--- testing/datagen_test.py
+++ testing/datagen_test.py
@@ -1,12 +1,12 @@
 import unittest
+
+from data_preparation.data_generator import DataGenerator
+from data_preparation.dataloader import normalise_input_array
 import pandas as pd
-from data_preparation.dataloader import get_previous_n_games, format_data, get_winstreak, normalise_ratings, \
-    get_ratings, get_mean_array, create_training_data, get_mean_stats, get_losestreak, normalise_input_array
-from pandas._testing import assert_frame_equal
 import numpy as np
 
 
-class TestDataloader(unittest.TestCase):
+class TestDataGenerator(unittest.TestCase):
 
     def setUp(self):
         self.data = pd.read_csv('test.csv')
@@ -14,36 +14,39 @@ class TestDataloader(unittest.TestCase):
     def test_previous_games(self):
         settings = {'n': 3, 'columns': 'pi-rating only', 'rating normalisation': 'min-max',
                     'combination': 'append'}
-        data = format_data(self.data, settings)
+        data_gen = DataGenerator(settings)
+        data = data_gen.format_data(data=self.data)
         selected_game = data[
             data[
                 'link'] == '/Matches/1080752/MatchReport/England-Premier-League-2016-2017-Crystal-Palace-Chelsea'].iloc[
             0]
-        previous = get_previous_n_games(data, 'Chelsea', settings['n'], selected_game)
+        previous = data_gen.get_previous_n_games(data, 'Chelsea', settings['n'], selected_game)
 
         actual_previous = pd.read_csv('previousgames.csv')
-        actual_previous = format_data(actual_previous, settings)
+        actual_previous = data_gen.format_data(actual_previous)
 
         np.testing.assert_array_equal(previous.values, actual_previous.values)
 
         selected_game = data[
             data['link'] ==
             '/Matches/1080798/MatchReport/England-Premier-League-2016-2017-Liverpool-Bournemouth'].iloc[0]
-        previous = get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
+        previous = data_gen.get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
         actual_previous = pd.read_csv('previousgames2.csv')
-        actual_previous = format_data(actual_previous, settings)
+        actual_previous = data_gen.format_data(actual_previous)
 
         np.testing.assert_array_equal(previous.values, actual_previous.values)
 
     def test_no_previous_games(self):
         settings = {'n': 3, 'columns': 'pi-rating only', 'rating normalisation': 'min-max',
                     'combination': 'append'}
-        data = format_data(self.data, settings)
+
+        data_gen = DataGenerator(settings)
+        data = data_gen.format_data(self.data)
 
         selected_game = data[
             data['link'] ==
             '/Matches/1080521/MatchReport/England-Premier-League-2016-2017-Liverpool-Middlesbrough'].iloc[0]
-        previous = get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
+        previous = data_gen.get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
 
         self.assertEqual(previous.size, 0)  # no previous game
 
@@ -51,28 +54,32 @@ class TestDataloader(unittest.TestCase):
         settings = {'n': 4, 'columns': 'pi-rating only', 'rating normalisation': 'min-max',
                     'combination': 'append'}
 
-        data = format_data(self.data, settings)
+        data_gen = DataGenerator(settings)
+        data = data_gen.format_data(self.data)
 
         selected_game = data[
             data[
                 'link'] == '/Matches/1080752/MatchReport/England-Premier-League-2016-2017-Crystal-Palace-Chelsea'].iloc[
             0]
-        self.assertEqual(get_winstreak(get_previous_n_games(data, 'Chelsea', settings['n'], selected_game),
-                                       'Chelsea'),
-                         3)
+        self.assertEqual(
+            data_gen.get_winstreak(data_gen.get_previous_n_games(data, 'Chelsea', settings['n'], selected_game),
+                                   'Chelsea'),
+            3)
 
     def test_get_rating(self):
         settings = {'n': 4, 'columns': 'pi-rating only', 'rating normalisation': 'min-max',
                     'combination': 'append'}
-        data = format_data(self.data, settings)
+        data_gen = DataGenerator(settings)
+        data = data_gen.format_data(self.data)
 
         selected_game = data[
             data['link'] == '/Matches/1080661/MatchReport/England-Premier-League-2016-2017-Liverpool-Chelsea'].iloc[0]
 
-        teama_previous = get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
-        teamb_previous = get_previous_n_games(data, 'Chelsea', settings['n'], selected_game)
+        teama_previous = data_gen.get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
+        teamb_previous = data_gen.get_previous_n_games(data, 'Chelsea', settings['n'], selected_game)
 
-        home_rating, away_rating = get_ratings('Liverpool', 'Chelsea', 'pi rating', teama_previous, teamb_previous)
+        home_rating, away_rating = data_gen.get_ratings('Liverpool', 'Chelsea', 'pi rating', teama_previous,
+                                                        teamb_previous)
 
         self.assertAlmostEqual(float(home_rating), 1.893333329)
         self.assertAlmostEqual(float(away_rating), 2.66508901)
@@ -80,16 +87,17 @@ class TestDataloader(unittest.TestCase):
     def test_normalise_ratings(self):
         settings = {'n': 4, 'columns': 'pi-rating only', 'rating normalisation': 'min-max',
                     'combination': 'append'}
-        data = format_data(self.data, settings)
+        data_gen = DataGenerator(settings)
+        data = data_gen.format_data(self.data)
 
         selected_game = data[
             data['link'] == '/Matches/1080661/MatchReport/England-Premier-League-2016-2017-Liverpool-Chelsea'].iloc[0]
 
-        teama_previous = get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
-        teamb_previous = get_previous_n_games(data, 'Chelsea', settings['n'], selected_game)
+        teama_previous = data_gen.get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
+        teamb_previous = data_gen.get_previous_n_games(data, 'Chelsea', settings['n'], selected_game)
 
-        away_rating, home_rating = normalise_ratings(data, 'Liverpool', 'Chelsea', settings, teama_previous,
-                                                     teamb_previous)
+        away_rating, home_rating = data_gen.normalise_ratings(data, 'Liverpool', 'Chelsea', teama_previous,
+                                                              teamb_previous)
 
         self.assertAlmostEqual(float(home_rating), 0.760428496)
         self.assertAlmostEqual(float(away_rating), 0.908354822)
@@ -98,16 +106,17 @@ class TestDataloader(unittest.TestCase):
         settings = {'n': 4, 'columns': 'both', 'rating normalisation': 'min-max',
                     'combination': 'append'}
 
-        data = format_data(self.data, settings)
+        data_gen = DataGenerator(settings)
+        data = data_gen.format_data(self.data)
 
         selected_game = data[
             data['link'] == '/Matches/1080661/MatchReport/England-Premier-League-2016-2017-Liverpool-Chelsea'].iloc[0]
 
-        teama_previous = get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
-        teamb_previous = get_previous_n_games(data, 'Chelsea', settings['n'], selected_game)
+        teama_previous = data_gen.get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
+        teamb_previous = data_gen.get_previous_n_games(data, 'Chelsea', settings['n'], selected_game)
 
-        away_rating, home_rating = normalise_ratings(data, 'Liverpool', 'Chelsea', settings, teama_previous,
-                                                     teamb_previous)
+        away_rating, home_rating = data_gen.normalise_ratings(data, 'Liverpool', 'Chelsea', teama_previous,
+                                                              teamb_previous)
 
         self.assertAlmostEqual(float(home_rating[0]), 0.81054513)
         self.assertAlmostEqual(float(away_rating[0]), 0.91689008)
@@ -119,22 +128,24 @@ class TestDataloader(unittest.TestCase):
         settings = {'n': 3, 'columns': 'both', 'rating normalisation': 'min-max',
                     'combination': 'append'}
 
-        data = format_data(self.data, settings)
+        data_gen = DataGenerator(settings)
+        data = data_gen.format_data(self.data)
 
         selected_game = data[
             data['link'] == '/Matches/1080661/MatchReport/England-Premier-League-2016-2017-Liverpool-Chelsea'].iloc[0]
 
-        previous_games = get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
-        array = get_mean_array('Liverpool', previous_games)
+        previous_games = data_gen.get_previous_n_games(data, 'Liverpool', settings['n'], selected_game)
+        array = data_gen.get_mean_array('Liverpool', previous_games)
 
-        np.testing.assert_array_equal(array, [6/3, 6/3, 0, 1])
+        np.testing.assert_array_equal(array, [6 / 3, 6 / 3, 0, 1])
 
     def test_create_training_data(self):
         settings = {'n': 3, 'columns': 'both', 'rating normalisation': 'min-max',
                     'combination': 'append'}
 
-        data = format_data(self.data, settings)
-        training_data = create_training_data(data,settings)
+        data_gen = DataGenerator(settings)
+        data = data_gen.format_data(self.data)
+        training_data = data_gen.create_training_data(data)
 
         # print(training_data)
 
@@ -143,9 +154,9 @@ class TestDataloader(unittest.TestCase):
                                       ['Chelsea', 'Man U', 7, 8, 9, 5]]),
                             columns=['home team', 'away team', 'home shots', 'home score',
                                      'away shots', 'away score'])
-
-        mean_data = get_mean_stats(data, 'Chelsea')
-        np.testing.assert_array_equal(mean_data, np.array([14/3, 15/3, 20/3]))
+        data_gen = DataGenerator(settings=None)
+        mean_data = data_gen.get_mean_stats(data, 'Chelsea')
+        np.testing.assert_array_equal(mean_data, np.array([14 / 3, 15 / 3, 20 / 3]))
 
     def test_get_losestreak(self):
         previous = pd.DataFrame(np.array([['Chelsea', 'Arsenal', 1, 2, 3, 10],
@@ -158,8 +169,8 @@ class TestDataloader(unittest.TestCase):
 
                                 columns=['home team', 'away team', 'home score', 'away score', 'home shots',
                                          'away shots'])
-
-        self.assertEqual(get_losestreak(previous, 'Arsenal'),
+        data_get = DataGenerator(settings=None)
+        self.assertEqual(data_get.get_losestreak(previous, 'Arsenal'),
                          3)
 
     def test_normalise_input_array(self):
@@ -195,8 +206,8 @@ class TestDataloader(unittest.TestCase):
         np.testing.assert_array_equal(y, real)
 
         x = np.array([[2, 0, 4, 1, 5, 4, 100, 280, 1.2232, 1.2245, 1.00264, 0.212],
-                  [6, 1, 0, 0, 2, 1, 96, 360, 1.456, 0.7856, 0.564, 1.456],
-                  [4, 7, 0, 4, 1, 0, 25, 125, 1.22555, 0.245, 0.222, 1.666]])
+                      [6, 1, 0, 0, 2, 1, 96, 360, 1.456, 0.7856, 0.564, 1.456],
+                      [4, 7, 0, 4, 1, 0, 25, 125, 1.22555, 0.245, 0.222, 1.666]])
 
         real = np.copy(x)
 
