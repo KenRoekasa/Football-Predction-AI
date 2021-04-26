01253dadf21b83e33844f304fee7b754b37ddd16
kxr758 <kxr758@student.bham.ac.uk>
2021-04-27 00:53:04 +0100

    Add better graph plotting

--- graphing/plot_graphs.py
+++ graphing/plot_graphs.py
@@ -3,37 +3,41 @@ import pandas as pd
 
 import os
 
-directory = "D:/Desktop/graphs/learning rate/batch/16/"
-for filename in os.listdir(directory):
-    if 'accuracy' in filename:
-        if 'train' in filename:
-            df = pd.read_csv(directory + filename)
-            plt.figure(0)
-            plt.plot(df['Step'], df['Value'], label='train')
-        if 'validation' in filename:
-            df = pd.read_csv(directory + filename)
-            plt.figure(0)
-            plt.plot(df['Step'], df['Value'], label='validation')
-    if 'loss' in filename:
-        if 'train' in filename:
-            df = pd.read_csv(directory + filename)
-            plt.figure(1)
-            plt.plot(df['Step'], df['Value'], label='train')
-        if 'validation' in filename:
-            df = pd.read_csv(directory + filename)
-            plt.figure(1)
-            plt.plot(df['Step'], df['Value'], label='validation')
+directories = ['D:/Desktop/graphs/final final/topology/order/%d/' % i for i in range(1,11)]
+print(directories)
+labels = [str(i) for i in range(1,11)]
 
+for idx, directory in enumerate(directories):
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            print(filename)
+            if 'accuracy' in filename:
+                if 'train' in filename:
+                    df = pd.read_csv(directory + filename)
+                    plt.figure(0)
+                    plt.plot(df['Step'], df['Value'], label='train')
+                if 'validation' in filename:
+                    df = pd.read_csv(directory + filename)
+                    plt.figure(0)
+                    plt.plot(df['Step'], df['Value'], label='validation', linestyle='dashed')
+            if 'loss' in filename:
+                if 'train' in filename:
+                    df = pd.read_csv(directory + filename)
+                    plt.figure(1)
+                    plt.plot(df['Step'], df['Value'], label='train')
+                if 'validation' in filename:
+                    df = pd.read_csv(directory + filename)
+                    plt.figure(1)
+                    plt.plot(df['Step'], df['Value'], label='validation',linestyle='dashed')
 
-plt.figure(0)
-plt.legend()
-plt.figure(1)
-plt.legend()
+    plt.figure(0)
+    plt.legend()
+    plt.figure(1)
+    plt.legend()
 
-plt.figure(0)
-plt.savefig(directory+'16-accuracy.png')
-plt.figure(1)
-plt.savefig(directory+'16-loss.png')
+    plt.figure(0)
+    plt.savefig(directory + str(labels[idx]) + 'accuracy.png')
+    plt.figure(1)
+    plt.savefig(directory + str(labels[idx]) + 'loss.png')
 
-plt.show()
-
+    plt.show()
