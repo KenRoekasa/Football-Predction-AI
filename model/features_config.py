05609c6950ed7f16f744b1a821234dd9a652aadf
kxr758 <kxr758@student.bham.ac.uk>
2021-05-07 02:47:37 +0100

    Add feature configuration file

---
+++ model/features_config.py
@@ -0,0 +1,34 @@
+features_dict = {
+    'self_selected': ['score', 'total shots', 'total conversion rate', 'open play shots', 'open play goals',
+                      'open play conversion rate', 'set piece shots', 'set piece goals', 'set piece conversion',
+                      'counter attack shots', 'counter attack goals', 'counter attack conversion', 'total passes',
+                      'total average pass streak', 'crosses', 'crosses average pass streak', 'through balls',
+                      'through balls average streak', 'long balls', 'long balls average streak', 'short passes',
+                      'short passes average streak', 'fouls', 'red cards', 'yellow cards', 'cards per foul', 'woodwork',
+                      'shots on target', 'shots off target', 'shots blocked', 'possession', 'touches', 'passes success',
+                      'accurate passes', 'key passes', 'dribbles won', 'dribbles attempted', 'dribbled past',
+                      'dribble success', 'aerials won', 'aerials won%', 'offensive aerials', 'defensive aerials',
+                      'successful tackles', 'tackles attempted', 'was dribbled', 'tackles success %', 'clearances',
+                      'interceptions', 'corners', 'corner accuracy', 'dispossessed', 'errors', 'offsides',
+                      'goals conceded',
+                      'win streak', 'lose streak', 'elo', 'pi rating'],
+
+    'feature_importance': ['score', 'total shots', 'total conversion rate', 'total passes', 'crosses', 'through balls',
+                           'long balls', 'short passes',
+                           'shots on target', 'shots off target', 'shots blocked', 'possession', 'touches',
+                           'passes success',
+                           'accurate passes', 'key passes', 'dribbles won', 'dribbles attempted', 'dribbled past',
+                           'dribble success', 'aerials won', 'aerials won%', 'offensive aerials', 'defensive aerials',
+                           'successful tackles', 'tackles attempted', 'was dribbled', 'tackles success %', 'clearances',
+                           'interceptions', 'corners', 'corner accuracy', 'dispossessed', 'errors', 'offsides',
+                           'goals conceded',
+                           'win streak', 'lose streak', 'elo', 'pi rating'],
+
+    'ratings': ['elo', 'pi rating'],
+    'simple shots': ['score', 'total shots', 'total conversion rate', 'goals conceded', 'win streak', 'lose streak',
+                     'elo', 'pi rating'],
+    'all': []
+
+}
+
+# print(len(features_dict['feature_importance']))
